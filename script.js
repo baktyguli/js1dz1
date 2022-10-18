6c3349f090f34b7a646670e2608ab8498e6cc8f2
baktyguli <115233442+baktyguli@users.noreply.github.com>
2022-10-18 16:15:40 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+//-------1-------//
+// const userName = prompt("ведите имя")
+// const Userlastname = prompt("ведите фамилия")
+
+// alert(" здравствуйте " + userName + Userlastname)
+// console.log(" здравствуйте " + userName + Userlastname);
+
+// ------2-----//
+
+// const num1 = prompt("ведите 1 число")
+// const num2 = prompt("ведите 2 число")
+
+// if (num1 < num2){
+//     alert(num1 + " меньше чем " + num2)
+// }else if (num2 < num1){
+//     alert(num2 + " меньше чем " + num1)
+// }else {
+//     alert("равны")
+// }
+
+//-------3------//
+
+// const colors = prompt("напиште цвет")
+
+// switch (colors) {
+//     case ("red"):
+//        alert("stop") 
+//         break;
+//     case ("yellow"):
+//         alert("wait")
+//         break;
+//     case ("green"):
+//         alert("go")
+//         break;
+//     default:
+//         alert("unknown color")
+//         break;
+// }
